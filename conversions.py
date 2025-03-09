a34d29a01cbc7ec65f5a658451d10268e3e04cb3
Traxster-TJ <Tyrelljackson3@gmail.com>
2025-03-09 18:39:12 -0400

    Update conversions.py

--- conversions.py
+++ conversions.py
@@ -1,13 +1,7 @@
-unit = input("Is this temperature in Celsius or Fahrenheit (C/F): ")
-temp = float(input("Enter the temperature: "))
+# tempconversions.py
 
-if unit == "C":
-    temp = round((9 * temp) / 5 + 32, 1)
-    print(f"The temperature in Fahrenheit is: {temp}°F")
+def convertCelsiusToKelvin(celsius):
+    return celsius + 273.15
 
-
-elif unit == "F":
-    temp = round((temp - 32) * 5 / 9, 1)
-    print(f"The temperature in Celsius is: {temp}°C")
-else:
-    print(f"{unit} is an invalid unit of measurement")
+def convertCelsiusToFahrenheit(celsius):
+    return celsius * 9/5 + 32
